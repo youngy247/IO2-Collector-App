baf314d9746f45fa646b5977a47c0de2629b1396
Adam Young <adamyoungy678@gmail.com>
2023-04-03 12:12:37 +0100

    exported whole MySql file for MatchAttax database

--- MatchAttaxDatabase.sql
+++ MatchAttaxDatabase.sql
@@ -1,28 +1,101 @@
-CREATE TABLE players (
-                           id INT(11) NOT NULL AUTO_INCREMENT,
-                           name VARCHAR(255) NOT NULL,
-                           dob DATE NOT NULL,
-                           country VARCHAR(255) NOT NULL,
-                           PRIMARY KEY (id)
-  );
+# ************************************************************
+# Sequel Ace SQL dump
+# Version 20046
+#
+# https://sequel-ace.com/
+# https://github.com/Sequel-Ace/Sequel-Ace
+#
+# Host: 127.0.0.1 (MySQL 5.5.5-10.11.2-MariaDB-1:10.11.2+maria~ubu2204)
+# Database: MatchAttax
+# Generation Time: 2023-04-03 10:19:42 +0000
+# ************************************************************
 
-CREATE TABLE player_stats (
-                                     id INT(11) NOT NULL AUTO_INCREMENT,
-                                     player_id INT(11) NOT NULL,
-                                     attack INT(11) NOT NULL,
-                                     defense INT(11) NOT NULL,
-                                     position VARCHAR(255) NOT NULL,
-                                     star_rating INT(11) NOT NULL,
-                                     rarity VARCHAR(255) NOT NULL,
-                                     PRIMARY KEY (id),
-                                     FOREIGN KEY (player_id) REFERENCES players(id)
-  );
 
-CREATE TABLE clubs (
-                              id INT(11) NOT NULL AUTO_INCREMENT,
-                              name VARCHAR(255) NOT NULL,
-                              PRIMARY KEY (id)
-  );
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE='NO_AUTO_VALUE_ON_ZERO', SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-ALTER TABLE players ADD COLUMN club_id INT(11) NOT NULL;
-ALTER TABLE players ADD FOREIGN KEY (club_id) REFERENCES clubs(id);
+
+# Dump of table clubs
+# ------------------------------------------------------------
+CREATE OR REPLACE DATABASE MatchAttax;
+
+USE MatchAttax;
+
+DROP TABLE IF EXISTS `clubs`;
+
+CREATE TABLE `clubs` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `name` varchar(255) NOT NULL,
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+LOCK TABLES `clubs` WRITE;
+/*!40000 ALTER TABLE `clubs` DISABLE KEYS */;
+
+INSERT INTO `clubs` (`id`, `name`)
+VALUES
+    (1,'Liverpool');
+
+/*!40000 ALTER TABLE `clubs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table player_stats
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `player_stats`;
+
+CREATE TABLE `player_stats` (
+                                `id` int(11) NOT NULL AUTO_INCREMENT,
+                                `player_id` int(11) NOT NULL,
+                                `attack` int(11) NOT NULL,
+                                `defense` int(11) NOT NULL,
+                                `position` varchar(255) NOT NULL,
+                                `star_rating` int(11) NOT NULL,
+                                `rarity` varchar(255) NOT NULL,
+                                PRIMARY KEY (`id`),
+                                KEY `player_id` (`player_id`),
+                                CONSTRAINT `player_stats_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+# Dump of table players
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `players`;
+
+CREATE TABLE `players` (
+                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) NOT NULL,
+                           `dob` date NOT NULL,
+                           `country` varchar(255) NOT NULL,
+                           `club_id` int(11) NOT NULL,
+                           PRIMARY KEY (`id`),
+                           KEY `club_id` (`club_id`),
+                           CONSTRAINT `players_ibfk_1` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+
+INSERT INTO `players` (`id`, `name`, `dob`, `country`, `club_id`)
+VALUES
+    (1,'John Smith','1993-04-02','England',1);
+
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
