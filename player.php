b56a0cc7338abd29e6e417bfdd6e9a02788936c6
Adam Young <adamyoungy678@gmail.com>
2023-04-03 15:34:03 +0100

    filled in body of constructor method in the player class

--- player.php
+++ player.php
@@ -15,7 +15,11 @@ class player
         string $name, int $attack, int $defence, int $club_id, int $position_id, int $id = -1
     )
     {
-
-
+        $this->id = $id;
+        $this->name = $name;
+        $this->attack = $attack;
+        $this->defence = $defence;
+        $this->club_id = $club_id;
+        $this->position_id = $position_id;
     }
 }
