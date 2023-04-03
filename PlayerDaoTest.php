d42a5daa2b8e535603e9e9887ebb0c287e837738
Adam Young <adamyoungy678@gmail.com>
2023-04-03 22:49:46 +0100

    set up function for unit tests for PlayerDao class

--- PlayerDaoTest.php
+++ PlayerDaoTest.php
@@ -4,5 +4,10 @@ use PHPUnit\Framework\TestCase;
 require_once 'PlayerDao.php';
 
 class PlayerDaoTest extends TestCase {
+    private PlayerDao $playerDao;
 
+    protected function setUp(): void
+    {
+        $this->playerDao = new PlayerDao();
+    }
 }
