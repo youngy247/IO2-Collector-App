d7b7462d8e942ed36f9a8d744a08f82a49de19a9
Adam Young <adamyoungy678@gmail.com>
2023-04-03 16:37:31 +0100

    updated s1task4 pdo file to match original

--- pdo-connection.php
+++ pdo-connection.php
@@ -1,41 +1,43 @@
 <?php
 
-// 1. Connect to database and save in a variable
-$host = 'db';
+const HOST = 'db';
+const USER = 'root';
+const password = 'password';
+const CHARSET = 'utf8mb4';
+
 $db = 'MatchAttax';
-$user = 'root';
-$password = 'password';
-$charset = 'utf8mb4';
+function connectToDb(string $db): PDO
+{
+    $dsn = "mysql:host=" . HOST . ";dbname=$db;charset=" . CHARSET;
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
 
-$options = [
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-];
+    try {
+        $pdo = new PDO($dsn, USER, password, $options);
+    } catch (PDOException $exception) {
+        echo '<p>There was an error connecting to the db</p>';
+        exit();
+    }
 
-try {
-    $pdo = new PDO($dsn, $user, $password, $options);
-} catch (\PDOException $exception) {
-    echo '<p>There was an error connecting to the db</p>';
-    exit();
+    return $pdo;
 }
 
+// Call connectToDb() to get a PDO object
+$pdo = connectToDb($db);
 
-// 2. Prepare Statement
-
+// Prepare SQL query
 $query = $pdo->prepare('SELECT `players`.`id`,`players`.`name`,`players`.`attack`, `players`.`defence`
-                        ,`clubs`.`name` as `club_name`, `position`.`name` as `position` FROM `players`
-                          INNER JOIN `clubs` ON `players`.`club_id` = `clubs`.`id`
+                        ,`clubs`.`name` as `club_name`, `position`.`name` as `position` FROM `players` 
+                          INNER JOIN `clubs` ON `players`.`club_id` = `clubs`.`id` 
                           INNER JOIN `position` ON `players`.`position_id` = `position`.`id`');
 
-
-// 3. Execute query
-
+// Execute query
 $query->execute();
 
-//Now we can get the result
-
+// Now we can get the result
 $result = $query->fetchAll();
 
 echo '<pre>';
