67049dc88f75361a91ea72a2914d6582eb1b9fb7
Adam Young <adamyoungy678@gmail.com>
2023-04-04 10:38:37 +0100

    Added get image in img tags

--- index.php
+++ index.php
@@ -58,7 +58,8 @@ $cloudinary->image('olympic_flag')->resize(Resize::fill(100, 150))->toUrl();
 
 $html = '';
 foreach ($players as $player) {
-    $html .= '<div class="player-card">'
+    $html .= '<div class="player-card">' . 
+        '<img>' . $player->getImage() . '<img/>'
         . '<p>Name: ' . $player->getName() . '</p>'
         . '<p>Attack: ' . $player->getAttack() . '</p>'
         . '<p>Defence: ' . $player->getDefence() . '</p>'
