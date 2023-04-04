9b0a366525a4ed0d9a4342e95356def953256b0d
Adam Young <adamyoungy678@gmail.com>
2023-04-04 09:54:02 +0100

    looped through all the players and displaying their stats

--- index.php
+++ index.php
@@ -7,6 +7,32 @@ $playerDao = new PlayerDao();
 $players = $playerDao->fetchAll();
 ?>
 
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Match Attax</title>
+
+        <meta name="description" content="Match Attax Collection">
+        <meta name="author" content="Adam Young">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/styles.css">
+
+        <link rel="icon" href="images/favicon.png" sizes="192x192">
+        <link rel="shortcut icon" href="images/favicon.png">
+        <link rel="apple-touch-icon" href="images/favicon.png">
+
+        <!-- <script defer src="js/index.js"></script> -->
+    </head>
+
+    <body>
+
+    <h1>Match Attax</h1>
+
+    <section class="collection">
 <?php
 use Cloudinary\Cloudinary;
 use Cloudinary\Transformation\Resize;
@@ -31,11 +57,13 @@ $cloudinary->image('olympic_flag')->resize(Resize::fill(100, 150))->toUrl();
 
 
 $html = '';
-foreach ($pigs as $pig) {
-    $html .= '<div class="pig-card">'
-        . '<p>Name: ' . $pig->getName() . '</p>'
-        . '<p>Weight: ' . $pig->getWeight() . '</p>'
-        . '<p>Colour: ' . $pig->getColour() . '</p>'
+foreach ($players as $player) {
+    $html .= '<div class="player-card">'
+        . '<p>Name: ' . $player->getName() . '</p>'
+        . '<p>Attack: ' . $player->getAttack() . '</p>'
+        . '<p>Defence: ' . $player->getDefence() . '</p>'
+        . '<p>Club: ' . $player->getClubId() . '</p>'
+        . '<p>Position: ' . $player->getPosition() . '</p>'
         . '</div>';
 }
 echo $html;
