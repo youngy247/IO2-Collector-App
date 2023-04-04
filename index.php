369a774b075848db30706d0a80cfb8ab3a834c87
Adam Young <adamyoungy678@gmail.com>
2023-04-04 15:25:24 +0100

    removed unneeded files

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . '/vendor/autoload.php');
+
 require_once 'PlayerDao.php';
 
 $playerDao = new PlayerDao();
@@ -19,27 +19,6 @@ require_once 'index.html';
 
     <section id="collection">
 <?php
-use Cloudinary\Cloudinary;
-use Cloudinary\Transformation\Resize;
-
-$cloudinary = new Cloudinary(
-    [
-        'cloud' => [
-            'cloud_name' => 'dhi7djro1',
-            'api_key'    => '611442853131594',
-            'api_secret' => 'IDAzt6eqFCUfUWh3V1XDMhd7Gug',
-        ],
-    ]
-);
-
-$cloudinary->uploadApi()->upload(
-    'https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg',
-    ['public_id' => 'olympic_flag']
-);
-
-$cloudinary->image('olympic_flag')->resize(Resize::fill(100, 150))->toUrl();
-
-
 
 $html = '<div class="card-grid">';
 foreach ($players as $player) {
