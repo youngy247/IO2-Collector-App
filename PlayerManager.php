cdefcadd9b17d9e6571384b7b5812ead8a14ef7d
Adam Young <adamyoungy678@gmail.com>
2023-04-05 12:17:25 +0100

    changed variable name in playerManager

--- PlayerManager.php
+++ PlayerManager.php
@@ -19,12 +19,12 @@ class PlayerManager
             !$player->getClubId() || !$player->getPositionId() || !$player->getImage()) {
             throw new InvalidArgumentException('Invalid player data');
         }
-        $stmt = $this->db->prepare(
+        $insert = $this->db->prepare(
             'INSERT INTO 
     players (name, attack, defence, club_id, position_id, image) 
              VALUES 
     (:name, :attack, :defence, :club_name, :position_id, :image)');
-        $stmt->execute([
+        $insert->execute([
             'name' => $player->getName(),
             'attack' => $player->getAttack(),
             'defence' => $player->getDefence(),
