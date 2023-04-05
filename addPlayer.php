410689adf1d0cc84f1e8ddd379353431d692d2ec
Adam Young <adamyoungy678@gmail.com>
2023-04-05 09:34:18 +0100

    insert the new player into the database

--- addPlayer.php
+++ addPlayer.php
@@ -13,10 +13,8 @@ $image = $_POST['image'];
 // Create a new Player object with the form data
 $player = new Player($image, $name, $attack, $defence, $club_id, $position_id);
 
+
 // Insert the new player into the database
-$db = new $pdo();
+$db = new Database();
 $db->insertPlayer($player);
 
-// Redirect the user to the collection page
-header('Location: index.php');
-exit();
