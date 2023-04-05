19c6ce1291ba3cb2445367e7afa04e51908c85bd
Adam Young <adamyoungy678@gmail.com>
2023-04-05 15:47:59 +0100

    created an if isset statement in addPlayer file

--- addPlayer.php
+++ addPlayer.php
@@ -4,12 +4,15 @@ require_once 'pdo-connection.php';
 require_once 'PlayerManager.php';
 
 // Retrieve the form data
-$name = $_POST['name'];
-$attack = $_POST['attack'];
-$defence = $_POST['defence'];
-$club_id = $_POST['club_id'];
-$position_id = $_POST['position_id'];
-$image = $_POST['image'];
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+    $attack = $_POST['attack'];
+    $defence = $_POST['defence'];
+    $club_id = $_POST['club_id'];
+    $position_id = $_POST['position_id'];
+    $image = $_POST['image'];
+}
+
 
 // Create a new Player object with the form data
 $player = new Player($image, $name, $attack, $defence, $club_id, $position_id);
