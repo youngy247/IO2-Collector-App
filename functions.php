e4732c891df6bcef208ee933e832990fb16f6e20
Adam Young <adamyoungy678@gmail.com>
2023-04-05 15:26:58 +0100

    added a loop for card numbers

--- functions.php
+++ functions.php
@@ -2,11 +2,12 @@
 
 function returnHtmlFromArray(array $players): string {
     $html = '<div class="card-grid">';
+    $cardId = 1;
     foreach ($players as $player) {
         $html .= '<div class="player-card">' .
             '<p class="player-name">'. $player->getName() .'</p>' .
             '<img src="'.$player->getImage().'" alt="A ' . $player->getName() .'">'
-            . '<p>Card number ' . $player->getId() . '</p>'
+            . '<p>Card number ' . $cardId++ . '</p>'
             . '<p>Attack: ' . $player->getAttack() . '</p>'
             . '<p>Defence: ' . $player->getDefence() . '</p>'
             . '<p>Club: ' . $player->getClubId() . '</p>'
