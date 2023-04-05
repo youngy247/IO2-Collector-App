8816369d86c0cefa3c5b491eadd2730fb92d068d
Adam Young <adamyoungy678@gmail.com>
2023-04-05 14:31:50 +0100

    put the names above the cards

--- functions.php
+++ functions.php
@@ -4,8 +4,9 @@ function returnHtmlFromArray(array $players): string {
     $html = '<div class="card-grid">';
     foreach ($players as $player) {
         $html .= '<div class="player-card">' .
+            '<p>'. $player->getName() .'</p>' .
             '<img src="'.$player->getImage().'" alt="A ' . $player->getName() .'">'
-            . '<p>Name: ' . $player->getName() . '</p>'
+//            . '<p>Name: ' . $player->getName() . '</p>'
             . '<p>Attack: ' . $player->getAttack() . '</p>'
             . '<p>Defence: ' . $player->getDefence() . '</p>'
             . '<p>Club: ' . $player->getClubId() . '</p>'
