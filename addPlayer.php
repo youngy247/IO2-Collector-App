b4b66ce8e371f02348fd3663195e602183f0a8b1
Adam Young <adamyoungy678@gmail.com>
2023-04-05 09:33:58 +0100

    creating new instance of Player

--- addPlayer.php
+++ addPlayer.php
@@ -9,3 +9,14 @@ $defence = $_POST['defence'];
 $club_id = $_POST['club_id'];
 $position_id = $_POST['position_id'];
 $image = $_POST['image'];
+
+// Create a new Player object with the form data
+$player = new Player($image, $name, $attack, $defence, $club_id, $position_id);
+
+// Insert the new player into the database
+$db = new $pdo();
+$db->insertPlayer($player);
+
+// Redirect the user to the collection page
+header('Location: index.php');
+exit();
